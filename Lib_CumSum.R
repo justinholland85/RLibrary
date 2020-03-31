95c8e120904e5ce8f80ab58f0e3c9f1c9662b25f
justinholland85 <36989967+justinholland85@users.noreply.github.com>
2020-04-01 10:42:39 +1100

    CumSum

--- Lib_CumSum.R
+++ Lib_CumSum.R
@@ -14,21 +14,46 @@ Lib.CumSum  <-  function(X){
 
   
 }
+######################################################################################################
+
+Lib.ProbMat <-  function(X, u){
+     
+     Nrow       <-  nrow(X)
+     Ncol       <-  ncol(X)
+     
+     if(missing(u)){ u <-  runif(Nrow)}
+     
+     
+     Y   <-  1 * (0 < u & u <= X[,1])
+     
+     for(i in 2:Ncol){
+          
+          Y  <- Y + i *  (X[,i-1] < u & u <= X[,i])
+          
+     }
+     
+     return(Y)
+     
+     
+}
+
+
 
 ######################################################################################################
-Lib.ProbMat <-  function(X, u){
+Lib.ProbVec <-  function(u, X, n){
   
-  Nrow       <-  nrow(X)
-  Ncol       <-  ncol(X)
+  N          <-  length(X)
+
+  if(missing(n)){n  <-  1}
+  if(missing(u)){u  <-  runif(n)}
   
-  if(missing(u)){ u <-  runif(Nrow)}
   
   
-  Y   <-  1 * (0 < u & u <= X[,1])
+  Y   <-  1 * (0 < u & u <= X[1])
   
-  for(i in 2:Ncol){
+  for(i in 2:N){
     
-    Y  <- Y + i *  (X[,i-1] < u & u <= X[,i])
+    Y  <- Y + i *  (X[i-1] < u & u <= X[i])
     
   }
   
@@ -36,3 +61,5 @@ Lib.ProbMat <-  function(X, u){
   
   
 }
+
+######################################################################################################
