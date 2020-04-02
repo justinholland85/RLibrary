bc1e359c75fa4bf00fd1eb389309800293bebd7a
justinholland85 <36989967+justinholland85@users.noreply.github.com>
2020-04-02 15:40:04 +1100

    LogScale.Smart

--- Lib_LogScale.R
+++ Lib_LogScale.R
@@ -10,3 +10,39 @@ Lib.LogScale  <-  function(X,Base){
      
 }
 
+######################################################################################################
+
+
+
+Lib.LogScale.Smart  <-  function(X,Base){
+  
+  if(missing(Base)){ Base <- 10}
+  
+  Min       <-  min(X)   
+  Max       <-  max(X)
+  
+  Type.Pos    <-  Min >= 0 
+  Type.Neg    <-  Max <= 0 
+  Type.PosNeg <-  Min < 0 & Max > 0
+  
+  Type        <-  c("Pos", "Neg", "PosNeg")[which(c(Type.Pos, Type.Neg, Type.PosNeg))]
+  
+  
+  if(Type %in% c("Pos", "Neg")){
+    
+    Y  <-  sign(X) * log(abs(X), Base)
+    
+  }
+  
+  if(Type == "PosNeg"){
+  
+  X[which( 0 < X & X < 1)]  <-  1
+  X[which(-1 < X & X < 0)]  <-  -1
+  
+  Y  <-  sign(X) * log(1 + abs(X), Base)
+  
+  }
+  
+  return(Y)
+  
+}
