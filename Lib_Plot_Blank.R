2ae1b56f94889f76a18f3a9acd8afb917bdcab7e
justinholland85 <36989967+justinholland85@users.noreply.github.com>
2020-04-20 13:31:36 +1000

    Update Lib_Plot_Blank.R

--- Lib_Plot_Blank.R
+++ Lib_Plot_Blank.R
@@ -7,8 +7,8 @@ Lib.Plot.Blank  <-  function(X.Lim, Y.Lim, X0, X1, Y0, Y1, X.Lab, Y.Lab, Main){
   if(missing(X1)){  X1      <-  X.Lim[2]}
   if(missing(Y0)){  Y0      <-  Y.Lim[1]}
   if(missing(Y1)){  Y1      <-  Y.Lim[2]}
-  if(missing(Xlab)){X.Lab    <-  ""}
-  if(missing(Ylab)){Y.Lab    <-  ""}
+  if(missing(X.Lab)){X.Lab  <-  ""}
+  if(missing(Y.Lab)){Y.Lab  <-  ""}
   if(missing(Main)){Main    <-  ""}
   
   plot(c(X0,X1), c(Y0,Y1), type='n', bty='n', xaxt ='n', yaxt='n', 
