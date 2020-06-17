1b30fdb2b591957b7341526c9e91f643cca34791
justinholland85 <36989967+justinholland85@users.noreply.github.com>
2020-06-17 11:13:03 +1000

    Update Lib_RangeMap.R

--- Lib_RangeMap.R
+++ Lib_RangeMap.R
@@ -1,38 +1,50 @@
-Lib.RangeMap  <-  function(X,R){
+# Lib.RangeMap  <-  function(X,R){
   
-  Y  <-  rep(0, length(X))
-  N  <-  length(R) - 1 
+#  Y  <-  rep(0, length(X))
+#  N  <-  length(R) - 1 
   
-  for(i in 1:N){
+#  for(i in 1:N){
     
-    if(R[i+1] == Inf){
+#    if(R[i+1] == Inf){
     
-    Y  <-  Y + i * (R[i] <= X & X <= R[i+1])
+#    Y  <-  Y + i * (R[i] <= X & X <= R[i+1])
       
-    } else {
+#    } else {
     
-    Y  <-  Y + i * (R[i] <= X & X < R[i+1])
+#    Y  <-  Y + i * (R[i] <= X & X < R[i+1])
     
-    }
+#    }
     
-  }
+#  }
   
-  return(Y)
+#  return(Y)
   
-}
+#}
 
 ######################################################################################################
 
-Lib.RangeMap.Cut  <-  function(X,R){ 
+Lib.RangeMap  <-  function(X,R){ 
 
 Cut     <-  cut(X, breaks = R, right = FALSE, include.lowest = TRUE )
 Levels  <-  levels(Cut)
 
 Value   <-  Lib.NA.To.Zero(as.numeric(Cut))
+Value[which(is.na(X))]  <-  NA
 
 return(Value)
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
