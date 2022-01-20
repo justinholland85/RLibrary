20c446a9f2d57d82c65cf3465f3df3029d67fa3f
justinholland85 <justin.holland85@gmail.com>
2022-01-20 14:38:43 +1100

    Lib.Append.DF

--- Lib_Rbind_DF.R
+++ Lib_Rbind_DF.R
@@ -24,6 +24,30 @@ Lib.Rbind.DF  <-  function(DF1, DF2){
 
 ######################################################################################################
 
+Lib.Add.DummyVars  <-  function(DF, AddVars){
+  
+  N.Vars            <-   length(AddVars)
+  N.Row             <-   nrow(DF)
+  
+  if(N.Vars > 0){
+  
+  Dummy.Add         <-  data.frame(rep(list(rep(NA, N.Row)), N.Vars))
+  names(Dummy.Add)  <-  AddVars
+  
+  DF.Out            <-  data.frame(DF, Dummy.Add)
+  
+  }  else { DF.Out  <-  DF}
+  
+  return(DF.Out)
+  
+}
+
+
+
+
+
+######################################################################################################
+
 
 Lib.ListBind.ByNames  <-  function(List, Names){
   
@@ -50,7 +74,29 @@ Lib.ListBind.ByNames  <-  function(List, Names){
   
 }
   
+######################################################################################################
+
+Lib.Append.DF    <-  function(DF1, DF2){
   
+  
+  DF1.a       <-  data.frame(lapply(DF1, Lib.FactorAsCharacter),       stringsAsFactors = FALSE)
+  DF2.a       <-  data.frame(lapply(DF2, Lib.FactorAsCharacter),       stringsAsFactors = FALSE)
+  
+  AllNames    <-  c(names(DF1.a), setdiff(names(DF2.a), names(DF1.a)))
+  
+  AddVars.1   <-  setdiff(names(DF2), names(DF1))
+  AddVars.2   <-  setdiff(names(DF1), names(DF2))
+  
+  DF1.a       <-  Lib.Add.DummyVars(DF1.a, AddVars.1)    
+  DF2.a       <-  Lib.Add.DummyVars(DF2.a, AddVars.2)    
+  
+  DF          <-  Lib.ListBind.ByNames(list(DF1.a, DF2.a), AllNames)
+  
+  return(DF)
+  
+}
+
+
   
   
   
