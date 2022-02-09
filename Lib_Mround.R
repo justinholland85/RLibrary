c3abea7a37d45d63aba4b6ff9f3d5dee635e85f6
justinholland85 <justin.holland85@gmail.com>
2022-02-09 11:03:00 +1100

    Update Lib_Mround.R
    Print Signif

--- Lib_Mround.R
+++ Lib_Mround.R
@@ -26,7 +26,7 @@ Lib.Mround  <-  function(X,M,Dir){
 
 Lib.PrintNumber   <-  function(X){
   
-  
+  # Note: not vectorised
   
   K1    <-  ceiling(log(X, 1000 ))
   
@@ -53,3 +53,106 @@ Lib.PrintNumber   <-  function(X){
   return(Y)
   
 }
+
+######################################################################################################
+
+Lib.PrintNumber   <-  function(X){
+  
+  # Note: not vectorised
+  
+  K1    <-  ceiling(log(X, 1000 ))
+  
+  
+  Parts    <-  list()
+  
+  for(i in 1:K1){
+    
+    Parts[[i]]   <-  floor(X  / (1000 ^ (i - 1))) %% 1000
+    
+  }
+  
+  
+  
+  Parts       <-  rev(Parts)
+  P1          <-  Parts[[1]]
+  
+  Parts       <-  lapply(Parts, Lib.LeadingZeros, 3)
+  Parts[[1]]  <-  P1
+  
+  
+  Y  <-  paste0(Parts, collapse = ",")
+  
+  return(Y)
+  
+}
+
+Lib.PrintNumber(3000123)
+
+
+
+X    <-  123456
+
+
+Lib.PrintSignif   <-  function(X, d){
+  
+  X   <-  signif(X, d)
+  
+  Mag    <-  floor(log(X, 10))
+  
+  D      <-  numeric(0)
+  M      <-  numeric(0)
+  
+  for(i in 1:d){
+    
+    M[[i]]    <-  Mag - (i-1)
+    
+    # some kind of floating point issue, the 12 may need to be fixed for small numbers
+    D[[i]]    <- floor( round(X %% (10 ^  (M[[i]] + 1)) / 10 ^ M[[i]], 12))
+    
+  }
+  
+  D[which(D == 10)]  <-  0
+  
+  Y  <-  as.character(D)
+  
+  DP    <-    match(0, M) 
+  
+  if(!is.na(DP) & DP  < d ){
+    
+    P0   <-  Y[1:DP]
+    P1   <-  Y[(DP+1):d]
+    
+    Y   <-  c(P0, ".", P1)
+    
+  }  
+  
+  
+  
+  if(M[1] < 0) {
+    
+    LZ     <-  c("0.", rep("0", abs(M[1]) - 1))
+    
+  } else { LZ  <-  character(0)}
+  
+  
+  if(M[d] > 0 ){ 
+    TZ   <-  rep("0", M[d])
+    
+  } else {TZ <-  character(0)}
+  
+  Z   <-  c(LZ, Y, TZ)
+  
+  Z   <-  paste0(Z, collapse = "")  
+  
+  
+  return(Z)
+  
+  
+}
+
+
+
+
+
+
+
