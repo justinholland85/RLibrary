9504a0229d274331cbff6f679cf474a5268f048a
justinholland85 <justin.holland85@gmail.com>
2023-03-16 08:23:20 +1100

    Update Lib_Integer64Fix.R

--- Lib_Integer64Fix.R
+++ Lib_Integer64Fix.R
@@ -2,7 +2,7 @@ Lib.Integer64Fix  <-  function(X){
   
 
   for(i in 1:ncol(X)){
-    if(class(X[[i]] == "integer64")){X[[i]]  <-  as.numeric(X[[i]])}
+    if(class(X[[i]]) == "integer64"){X[[i]]  <-  as.numeric(X[[i]])}
     
   }
   
