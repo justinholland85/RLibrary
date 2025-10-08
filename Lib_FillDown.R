c9f9da0e35dae9e6c6717a01341f13741f2876bf
justinholland85 <justin.holland85@gmail.com>
2025-10-08 14:27:13 +1100

    Fix Fill Down

--- Lib_FillDown.R
+++ Lib_FillDown.R
@@ -3,7 +3,11 @@ Lib.FillDown  <-  function(X, Y){
   # X is the vector to fill down
   # Y is a boolean vector taking 1 where the spot should be filled from above
   
-  X[Y == 1]  <-  X[cumsum(Y == 0)][Y == 1]
+  CumSum   <-  cumsum(!Y)
+  Match    <-  match(CumSum, CumSum)
+  X[Match]
   
   return(X)
 }
+
+
