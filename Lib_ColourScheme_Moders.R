8814f2762c8dd9608e17d5dfedcea2706c540c81
justinholland85 <36989967+justinholland85@users.noreply.github.com>
2025-11-11 14:18:24 +1100

    Update Lib_ColourScheme_Moders.R

--- Lib_ColourScheme_Moders.R
+++ Lib_ColourScheme_Moders.R
@@ -115,3 +115,39 @@ Lib.ColourScheme.Values  <-  function(Base){
   return(DF)
   
 }
+
+
+
+#====================================================================================================#
+# Lib.Plot.Colours   
+#====================================================================================================#
+
+Lib.Plot.Colours   <- function(Colours,
+                               Plot.New = TRUE,
+                               X0 = 0, X1 = 1, Y0 = 0, Y1 = 1){
+  
+  
+  if(Plot.New == 1){Lib.Plot.Blank(X0 = X0, X1 = X1, Y0 = Y0, Y1 = Y1)}
+  
+  N.y      <-  nrow(Colours)
+  N.x      <-  ncol(Colours)
+  
+  Grid      <-  Lib.Grid.Coords(N.x, N.y, X0 = X0, X1 = X1, Y0 = Y0, Y1 = Y1)
+  
+  
+  for(i in 1:N.y){
+    for(j in 1:N.x){
+      
+      
+      x0    <-  Grid$X[j]
+      x1    <-  Grid$X[j+1]
+      y0    <-  Grid$Y[i+1]
+      y1    <-  Grid$Y[i]
+      
+      
+      polygon(x = c(x0, x0, x1, x1), 
+              y = c(y0, y1, y1, y0), col = Colours[i,j])
+      
+    } }
+  
+}
